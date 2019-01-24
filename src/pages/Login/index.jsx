61d0dbfeaf3317f8d66bb62f43304d3408fce245
Petar Slovic <petars@Petars-MacBook-Pro-2.local>
2019-01-24 18:43:12 +0100

    merge and login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import firebase from '../../services/firebase'
 import logo from '../../img/logo.png'
 import { withRouter } from 'react-router-dom'
+import { ReactComponent as FacebookIcon } from './facebook.svg';
+import { ReactComponent as GoogleIcon } from './google.svg';
 
 class Login extends React.Component {
   loginGoogle () {
@@ -24,7 +26,7 @@ class Login extends React.Component {
         })
 
   }
-  
+
   loginFacebook () {
     let facebookAuthProvider = new firebase.auth.FacebookAuthProvider()
 
@@ -51,8 +53,15 @@ class Login extends React.Component {
         <div className="columns is-gapless is-fullheight">
           <img className="login-logo" src={logo} alt="logo" />
           <div className="column">
-            <button className="button login-button is-success" onClick={() => this.loginGoogle()}>Login with Google</button>
-            <button className="button login-button is-success" onClick={() => this.loginFacebook()}>Login with Facebook</button>
+            <button className="button login-button login-button--google is-success" onClick={() => this.loginGoogle()}>
+              <GoogleIcon />
+              <span className="login-button-text">Google</span>
+            </button>
+
+            <button className="button login-button login-button--facebook is-success" onClick={() => this.loginFacebook()}>
+              <FacebookIcon />
+              <span className="login-button-text">Facebook</span>
+            </button>
           </div>
         </div>
       )
